1153b6690a40074e4c7e5355e85eb06dcb7a3f0c
toma-sh <zoltom83@gmail.com>
2023-06-14 19:08:09 +0200

    Adding NWD function declaration and definition

---
+++ nwd.c
@@ -0,0 +1,23 @@
+#include <stdio.h>		
+
+int NWD(int a,int b);	
+
+int main()				
+{
+	int a,b;			
+
+	printf("Podaj dwie liczby calkowite oddzielone spacja: ");	
+	scanf("%d%d",&a,&b);	
+				
+	NWD(a,b);							
+
+    return 0;
+}
+
+int NWD(int a,int b)		
+{
+
+	
+	
+	return 0;	
+}
